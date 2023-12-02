df33112c6254e9866f1a22e18ec23c5c74ccc71f
Neralis <coopmaster333@gmail.com>
2023-12-02 14:34:30 +0300

    Initial commit

---
+++ test2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pC1DataSet = new test2.PC1DataSet();
+            this.computersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.computersTableAdapter = new test2.PC1DataSetTableAdapters.ComputersTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pC1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(845, 266);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // pC1DataSet
+            // 
+            this.pC1DataSet.DataSetName = "PC1DataSet";
+            this.pC1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // computersBindingSource
+            // 
+            this.computersBindingSource.DataMember = "Computers";
+            this.computersBindingSource.DataSource = this.pC1DataSet;
+            // 
+            // computersTableAdapter
+            // 
+            this.computersTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 293);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(168, 293);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pC1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PC1DataSet pC1DataSet;
+        private System.Windows.Forms.BindingSource computersBindingSource;
+        private PC1DataSetTableAdapters.ComputersTableAdapter computersTableAdapter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ test2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection PC1 = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "pC1DataSet.Computers". При необходимости она может быть перемещена или удалена.
+            this.computersTableAdapter.Fill(this.pC1DataSet.Computers);
+
+            PC1 = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\kosty\\source\\repos\\PC1.mdf;Integrated Security=True;Connect Timeout=30");
+            PC1.Open();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Computers", PC1);
+            DataSet db = new DataSet();
+            dataAdapter.Fill(db);
+            dataGridView1.DataSource = db.Tables[0];
+
+            // Заполняем ComboBox названиями столбцов
+            foreach (DataColumn column in db.Tables[0].Columns)
+            {
+                comboBox1.Items.Add(column.ColumnName);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string columnName = comboBox1.SelectedItem.ToString();
+                string filterExpression = $"{columnName} LIKE '%{textBox1.Text}%'";
+
+                // Проверяем, является ли столбец числовым
+                if (dataGridView1.Columns[columnName].ValueType == typeof(int) || dataGridView1.Columns[columnName].ValueType == typeof(decimal))
+                {
+                    // Если столбец числовой, используем метод Convert.ToString() для преобразования числа в строку перед использованием LIKE
+                    filterExpression = $"Convert({columnName}, 'System.String') LIKE '%{textBox1.Text}%'";
+                }
+
+                (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = filterExpression;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // При изменении выбранного столбца вызываем обработчик изменения текста для обновления фильтрации
+            textBox1_TextChanged(sender, e);
+        }
+    }
+}
